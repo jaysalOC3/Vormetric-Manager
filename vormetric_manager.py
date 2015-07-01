77f3bd6c57cd1ca5ed59d3208be3a262e53d7dab
jaysalOC3 <jaysal3@gmail.com>
2015-07-01 09:40:14 -0700

    Added a check for enc temp folder.

--- vormetric_manager.py
+++ vormetric_manager.py
@@ -10,11 +10,17 @@ else:
     print "Please install python 2.7 to use this script."
     exit(1)
 ###
-import shutil
+import shutil, os
 
 ROOT_FOLDER = '/pmdms_sec_test'
-
+ENC_FOLDER = ROOT_FOLDER + '/enc'
 ###
 # Check the root folder to encrypt
+if os.path.exists(ENC_FOLDER):
+    print "Found Folder: enc"
+else:
+    print "Could not find enc folder: Exit"
+    exit(1)
+
 
 ###
